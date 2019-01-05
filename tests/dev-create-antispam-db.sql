5a9dc1fcae1db572209515398550ba922b6f2b45
Ondrej Vasko <ondrej.vaskoo@gmail.com>
2019-01-05 17:49:56 +0100

    Create docker-compose for easy development environment bootstrap
    Create docker-compose configuration, sample postfwd and postfwd antispam
    configuration together with initial mysql database creation script.
    
    This allows developers to simply create development environment on local
    computer and actively develop and test postfwd plugin.
    
    The postfwd branch is frozen to version `v1.37`.
    
    Signed-off-by: Ondrej Vasko <ondrej.vaskoo@gmail.com>

---
+++ tests/dev-create-antispam-db.sql
@@ -0,0 +1,10 @@
+USE postfwd-antispam-test;
+CREATE TABLE IF NOT EXISTS postfwd_logins (
+   sasl_username varchar(100),
+   ip_address varchar(16),
+   state_code varchar(4),
+   login_count int,
+   last_login timestamp
+);
+CREATE INDEX postfwd_sasl_client_state_index ON postfwd_logins (sasl_username, ip_address, state_code);
+CREATE INDEX postfwd_sasl_username ON postfwd_logins (sasl_username);
