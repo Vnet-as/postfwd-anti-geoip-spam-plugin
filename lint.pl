ad2978bfdd4b9f3aba8dc6e846acc36dd6164781
Ondrej Vasko <ondrej.vaskoo@gmail.com>
2019-01-06 12:23:38 +0100

    Remove lint.pl script and replace it with RC file for perl critic
    This is more general solution.
    
    Lint can now be executed easily with command `perlcritic <file>`,
    so for example `perlcritic postfwd-anti-spam.plugin`
    
    Signed-off-by: Ondrej Vasko <ondrej.vaskoo@gmail.com>

--- lint.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-use Perl::Critic;
-
-my $file = "postfwd-anti-spam.plugin";
-my $critic = Perl::Critic->new();
-my @violations = $critic->critique($file);
-print @violations;
